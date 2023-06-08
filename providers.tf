b4779085757802c9b8ec3d711a98bf2eb637058c
Alexander Perez <perezalex486@gmail.com>
2023-06-08 10:38:08 -0500

    se agrega bucket para almacenar archivo state

--- providers.tf
+++ providers.tf
@@ -10,3 +10,12 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+
+terraform {
+  backend "s3" {
+    bucket         = "state-terraform-chl"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
